e499d4ab5b4b406452de31400cbc71d0339569a4
Losacco Rachel <losacco@keeten.strw.leidenuniv.nl>
2019-05-17 15:40:24 +0200

    galaxy_merger.py successfully runs and outputs .dat files. Next step: plot these data.

---
+++ FinalProject/README.md
@@ -0,0 +1,4 @@
+galaxy_merger.py currently outputs information into 4 .dat files tracking the
+x and y positions of 2000 particles in the Milky Way (MW) and Andromeda (And)
+for 10 time steps. One file indicates the dimension and galaxy, then records
+2000 columns for each particle and 10 rows for each time step.
--- FinalProject/galaxy_merger.py
+++ FinalProject/galaxy_merger.py
@@ -42,16 +42,17 @@ def create_two_galaxies(M_MW, M_And, R_MW, R_And,
     return MW_galaxy, And_galaxy
 
 def simulate_merger(MW_galaxy, And_galaxy, nhalo_MW, nhalo_And,
-                    time_step, t_end):
+                    M_MW, R_MW, time_step, t_end):
     '''
     Simulate the merger of MW_galaxy and And_galaxy until t_end
     These galaxies have a halo particle number nhalo_[galaxy]
     '''
     # Define units of the system
-    converter = ConvertBetweenGenericAndSiUnits(constants.c, units.s)
+    converter_generic = ConvertBetweenGenericAndSiUnits(constants.c, units.s)
+    converter = nbody_system.nbody_to_si(M_MW, R_MW)
     # Establish hydrodynamics solver
     hydro = Gadget2(converter)
-
+    hydro.parameters.time_max = 2*converter_generic.to_generic(t_end)
     # Add galaxies to solver
     Milky_Way = hydro.particles.add_particles(MW_galaxy)
     Andromeda = hydro.particles.add_particles(And_galaxy)
@@ -74,7 +75,9 @@ def simulate_merger(MW_galaxy, And_galaxy, nhalo_MW, nhalo_And,
     y_MW = []
     x_And = []
     y_And = []
+    t = []
     while time < t_end:
+        t.append(time)
         time += time_step
         # Evolve the simulation by one time step
         hydro.evolve_model(time)
@@ -87,48 +90,53 @@ def simulate_merger(MW_galaxy, And_galaxy, nhalo_MW, nhalo_And,
         x_MW.append(MW_disk.x.value_in(units.kpc))
         y_MW.append(MW_disk.y.value_in(units.kpc))
         x_And.append(And_disk.x.value_in(units.kpc))
-        y_And.append(And_dick.y.value_in(units.kpc))
+        y_And.append(And_disk.y.value_in(units.kpc))
 
     hydro.stop()
 
     # Create animated plot of merger
-    FFMpegWriter = manimation.writers['ffmeg']
-    metadata = dict(title = 'Galaxy Collision',
-                    artist = 'Computational Physics',
-                    comment = 'Computational Physcs')
-    writer = FFMpegWriter(fps=30, metadata=metadata)
-
-    fig = plt.figure()
-    l, = plt.plot([],[], 'k-o')
-    nsteps = int(t_end/time_step)
-
-    with writer.savefig(fig, 'output.mp4', nsteps):
-        for i in range(nsteps):
-            plt.clf()
-            plt.xlim()
-            plt.ylim()
-            plt.grid()
-            plt.scatter(x_MW, y_MW, 'r', label='Milky Way')
-            plt.scatter(x_And, y_And, 'b', label='Andromeda')
-
-            writer.grab_frame()
+##    FFMpegWriter = manimation.writers['ffmeg']
+##    metadata = dict(title = 'Galaxy Collision',
+##                    artist = 'Computational Physics',
+##                    comment = 'Computational Physcs')
+##    writer = FFMpegWriter(fps=30, metadata=metadata)
+##
+##    fig = plt.figure()
+##    l, = plt.plot([],[], 'k-o')
+##    nsteps = int(t_end/time_step)
+##
+##    with writer.savefig(fig, 'output.mp4', nsteps):
+##        for i in range(nsteps):
+##            plt.clf()
+##            plt.xlim()
+##            plt.ylim()
+##            plt.grid()
+##            plt.scatter(x_MW, y_MW, 'r', label='Milky Way')
+##            plt.scatter(x_And, y_And, 'b', label='Andromeda')
+##
+##            writer.grab_frame()
+    return x_MW, y_MW, x_And, y_And
 
 if __name__ == '__main__':
     M_MW = 10**12. | units.MSun
     M_And = 10**12. | units.MSun
     R_MW = 30.660 | units.kpc
     R_And = 33.726 | units.kpc
-    nhalo_MW = 20000
-    nhalo_And = 20000
-    nbulge_MW = 10000
-    nbulge_And = 10000
-    ndisk_MW = 10000
-    ndisk_And = 10000
+    nhalo_MW = 2000
+    nhalo_And = 2000
+    nbulge_MW = 1000
+    nbulge_And = 1000
+    ndisk_MW = 1000
+    ndisk_And = 1000
     time_step = 1 | units.Myr
     t_end = 10 | units.Myr
 
     MW_galaxy, And_galaxy = create_two_galaxies(M_MW, M_And, R_MW, R_And,
                         nhalo_MW, nhalo_And, nbulge_MW, nbulge_And,
                         ndisk_MW, ndisk_And)
-    simulate_merger(MW_galaxy, And_galaxy, nhalo_MW, nhalo_And,
-                    time_step, t_end)
+    x_MW, y_MW, x_And, y_And = simulate_merger(MW_galaxy, And_galaxy, nhalo_MW, nhalo_And,
+                    M_MW, R_MW, time_step, t_end)
+    np.savetxt('out_x_MW.dat', x_MW)
+    np.savetxt('out_y_MW.dat', y_MW)
+    np.savetxt('out_x_And.dat', x_And)
+    np.savetxt('out_y_And.dat', y_And)
