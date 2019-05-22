76f2f1843a5f25b1ff0f95eb71572b2630320b44
Losacco Rachel <losacco@keeten.strw.leidenuniv.nl>
2019-05-22 12:46:40 +0200

    100 time steps, new initial position, tracking energies

--- FinalProject/README.md
+++ FinalProject/README.md
@@ -3,6 +3,8 @@ x and y positions of 40.000 particles in the Milky Way (MW) and Andromeda (And)
 for 100 time steps. One file indicates the dimension and galaxy, then records
 40.000 columns for each particle and 100 rows for each time step.
 
+It also creates a plot of the kinetic, potential, and total energy in 'energy_plt.png'.
+
 ./scrap/ has test files. Other files are necessary for running AMUSE, do not
 remove or move them from this directory.
 
--- FinalProject/galaxy_merger.py
+++ FinalProject/galaxy_merger.py
@@ -11,22 +11,28 @@ from amuse.units import units, constants
 from amuse.units.optparse import OptionParser
 from amuse.lab import nbody_system
 from amuse.ext.galactics_model import new_galactics_model
-from amuse.lab import Gadget2
+from amuse.community.gadget2.interface import Gadget2
 from amuse.units.generic_unit_converter import ConvertBetweenGenericAndSiUnits
 
 def make_plot(disk1, disk2, state):
+    '''
+    Scatter plot of disk & bulge particles of two galaxies at a given
+    time ("start" or "end" state) of the simulation
+    For a quick simulation
+    '''
     plt.scatter(disk1.x.value_in(units.kpc), disk1.y.value_in(units.kpc),
                 color='r', s=1, label='Milky Way')
     plt.scatter(disk2.x.value_in(units.kpc), disk2.y.value_in(units.kpc),
                 color='b', s=1, label='Andromeda')
-    plt.xlim(-300, 300)
+##    plt.xlim(-300, 300)
     plt.xlabel('$x$ [kpc]')
-    plt.ylim(-300, 300)
+##    plt.ylim(-300, 300)
     plt.ylabel('$y$ [kpc]')
     plt.title('Merger at '+state)
     plt.legend()
     plt.savefig('merger_plot_'+state+'.png')
     plt.close()
+    print('Plot made')
 
 def create_two_galaxies(M_MW, M_And, R_MW, R_And,
                         n_halo, n_bulge, n_disk):
@@ -50,14 +56,14 @@ def create_two_galaxies(M_MW, M_And, R_MW, R_And,
     MW_galaxy.mass = M_MW
     And_galaxy.mass = M_And
     # Offset
-    And_galaxy.position = MW_galaxy.position
-    MW_galaxy.position += [100.0, 100.0, 0.0] | units.kpc
-    MW_galaxy.rotate(0., np.pi/2, np.pi/4)
-    MW_galaxy.velocity += [-10.0, 0.0, -10.0] | units.km/units.s
+    MW_galaxy.move_to_center()
+    And_galaxy.position = MW_galaxy.position.value_in(units.kpc) \
+                          - [700.0, 500.0, 0.0] | units.kpc
     
-    And_galaxy.position -= [100.0, 0.0, 0.0] | units.kpc
-    And_galaxy.rotate(np.pi/4., np.pi/4., 0.0)
-    And_galaxy.velocity -= [0.0, 0.0, 0.0] | units.km/units.s
+    MW_galaxy.rotate(0., np.pi/2, np.pi/4)
+    MW_galaxy.velocity += [-20.0, -20.0, 0.0] | units.km/units.s
+    And_galaxy.rotate(np.pi/4, np.pi/4, 0.0)
+    And_galaxy.velocity += [0.0, 0.0, 0.0] | units.km/units.s
 
     return MW_galaxy, And_galaxy
 
@@ -82,10 +88,12 @@ def simulate_merger(MW_galaxy, And_galaxy, n_halo,
     MW_disk = Milky_Way[:n_halo]
     And_disk = Andromeda[:n_halo]
 
-##    # Quick run
-##    make_plot(MW_disk, And_disk, 'start')
+    # Quick run
+    # Do not run simulation again after this
+    make_plot(MW_disk, And_disk, 'start')
 ##    hydro.evolve_model(t_end)
 ##    make_plot(MW_disk, And_disk, 'end')
+    
     # Run simulation
     hydro.timestep = time_step
     time = 0.0 | units.Myr
@@ -95,9 +103,13 @@ def simulate_merger(MW_galaxy, And_galaxy, n_halo,
     x_And = []
     y_And = []
     t = []
+    # Also record energy
+    KE = [] # kinetic
+    PE = [] # inteneral
+    TotE = [] # total
     while time < t_end:
-        t.append(time)
-        time += time_step
+        t.append(time.value_in(units.Myr))
+        print(time)
         # Evolve the simulation by one time step
         hydro.evolve_model(time)
 
@@ -106,9 +118,31 @@ def simulate_merger(MW_galaxy, And_galaxy, n_halo,
         y_MW.append(MW_disk.y.value_in(units.kpc))
         x_And.append(And_disk.x.value_in(units.kpc))
         y_And.append(And_disk.y.value_in(units.kpc))
+        kin = hydro.kinetic_energy.value_in(units.J)
+        KE.append(kin)
+        pot = hydro.potential_energy.value_in(units.J)
+        PE.append(pot)
+        TotE.append(kin + pot)
+        
+        time += time_step
 
     hydro.stop()
-    return x_MW, y_MW, x_And, y_And
+    print('Simulation complete')
+
+    # Plot energies
+    plt.plot(t, KE, label='Kinetic Energy')
+    plt.plot(t, PE, label='Potential Energy')
+    plt.plot(t, TotE, label='Total Energy')
+    plt.plot((min(t), max(t)),(0,0), 'k--')
+    plt.xlabel('Time (Myr)')
+    plt.ylabel('Energy (J)')
+    plt.title('Change In Energy of System')
+    plt.legend()
+    plt.savefig('energy_plot.png')
+    plt.close()
+    print('Energy plot complete')
+    
+    return x_MW, y_MW, x_And, y_And, t, KE, PE, TotE
 
 def new_option_parser():
     from amuse.units.optparse import OptionParser
@@ -132,10 +166,10 @@ def new_option_parser():
     result.add_option("--n_halo", dest="n_halo", default = 20000,
                       help="number of stars in the halo [%default]")
     result.add_option("--t_end", unit=units.Myr,
-                      dest="t_end", default = 200|units.Myr,
+                      dest="t_end", default = 10|units.Myr,
                       help="End of the simulation [%default]")
     result.add_option("--t_step", unit=units.Myr,
-                      dest="time_step", default = 2|units.Myr,
+                      dest="time_step", default = 0.1|units.Myr,
                       help="Time step for simulation [%default]")
     return result    
 
@@ -144,13 +178,18 @@ if __name__ == '__main__':
     MW_galaxy, And_galaxy = create_two_galaxies(o.M_MW, o.M_And,
                                                 o.R_MW, o.R_And,
                                                 o.n_halo, o.n_bulge, o.n_disk)
+    # For a quick simulation
 ##    simulate_merger(MW_galaxy, And_galaxy, o.n_halo, o.n_halo,
 ##                    o.M_galaxy, o.R_galaxy, o.t_end)
-##
 
-    x_MW, y_MW, x_And, y_And = simulate_merger(MW_galaxy, And_galaxy,
-                                               o.n_halo, o.time_step, o.t_end)
+    x_MW, y_MW, x_And, y_And, t, KE, PE, TotE = \
+          simulate_merger(MW_galaxy, And_galaxy, o.n_halo, o.time_step, o.t_end)
     np.savetxt('out_x_MW.dat', x_MW)
     np.savetxt('out_y_MW.dat', y_MW)
     np.savetxt('out_x_And.dat', x_And)
     np.savetxt('out_y_And.dat', y_And)
+    np.savetxt('time.dat', t)
+    np.savetxt('KE.dat', KE)
+    np.savetxt('PE.dat', PE)
+    np.savetxt('TotE.dat', TotE)
+    print('Files created')
